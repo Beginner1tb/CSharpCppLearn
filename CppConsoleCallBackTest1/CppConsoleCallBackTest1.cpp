c29d38f75867da21acbcf739326315669711e61e
bbbbb <bbbbb@DESKTOP-ELI79IJ>
2024-10-19 16:06:44 +0800

    回调函数学习

---
+++ CppConsoleCallBackTest1/CppConsoleCallBackTest1.cpp
@@ -0,0 +1,24 @@
+﻿// CppConsoleCallBackTest1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <functional>
+
+//Modern Usage
+void doTask(std::function<void(int)> callback) {
+    std::cout << "Task is running..." << std::endl;
+    callback(42);
+}
+
+
+int main()
+{
+    auto myCallback = [](int result) {
+        std::cout << "Lambda callback executed with result: " << result << std::endl;
+    };
+
+    doTask(myCallback);
+    return 0;
+    std::cout << "Hello World!\n";
+}
+
